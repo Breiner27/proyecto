8909aab7ab44cd4ceb0a976b277961a9f3df5567
breiner <breiner-rojas15@hotmail.com>
2024-11-26 21:53:29 -0500

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./ejemplo/header/header.component";
+import { NavComponent } from "./ejemplo/nav/nav.component";
+import { CuerpoComponent } from "./ejemplo/cuerpo/cuerpo.component";
+import { FooterComponent } from "./ejemplo/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, CuerpoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto';
+  title = 'Curso Angular';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CuerpoComponent } from './ejemplo/cuerpo/cuerpo.component';
+import { HeaderComponent } from './ejemplo/header/header.component';
+import { FooterComponent } from './ejemplo/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:CuerpoComponent},
+    {path:'',component:HeaderComponent},
+    {path:'',component:FooterComponent}
+];
