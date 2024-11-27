6df97b3dd8402d25a57025dd182224a0020bdd56
breiner <breiner-rojas15@hotmail.com>
2024-11-26 20:36:55 -0500

    primer commit

--- src/app/ejemplo/cuerpo/cuerpo.component.ts
+++ src/app/ejemplo/cuerpo/cuerpo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PadreComponent } from "../padre/padre.component";
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [],
+  imports: [PadreComponent],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
--- src/app/usuario/hijo/hijo.component.ts
+++ src/app/usuario/hijo/hijo.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hijo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './hijo.component.html',
   styleUrl: './hijo.component.css'
 })
 export class HijoComponent {
+  @Output() mensajeDesdeHijo = new EventEmitter<string>()
+  @Output() incrementarDesdeHijo = new EventEmitter<void>()
+  @Output() disminuirDesdeHijo = new EventEmitter<void>()
 
+
+  mensaje: string = '';
+
+  enviarMensaje() {
+    this.mensajeDesdeHijo.emit(this.mensaje)
+  }
+
+  incrementar() {
+    this.incrementarDesdeHijo.emit();
+  }
+
+  disminuir() {
+    this.disminuirDesdeHijo.emit();
+  }
 }
