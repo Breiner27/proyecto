2109383642a10c07941c8d352265b4ff24ea6417
breiner <breiner-rojas15@hotmail.com>
2024-11-27 20:36:11 -0500

    beji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "./ejemplo/header/header.component";
 import { NavComponent } from "./ejemplo/nav/nav.component";
 import { CuerpoComponent } from "./ejemplo/cuerpo/cuerpo.component";
 import { FooterComponent } from "./ejemplo/footer/footer.component";
+import { IniciarsesionComponent } from "./beji/iniciarsesion/iniciarsesion.component";
+import { PagPrincipalComponent } from "./beji/pag-principal/pag-principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavComponent, CuerpoComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, CuerpoComponent, FooterComponent, IniciarsesionComponent, PagPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
